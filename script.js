b3ecb8ebc72ddf336ad5dddcbf84cc6c950acec8
Luan Santos <luan.cardoso0029@gmail.com>
2023-06-15 13:12:22 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function clickMenu() {
+    var itensBurg = document.getElementById("itensBurg");
+    var barra = document.getElementById("barra");
+    var fechar = document.getElementById("fechar");
+    
+    if (itensBurg.style.display === 'none') {
+        itensBurg.style.display = 'block';
+        barra.style.display = 'none';
+        fechar.style.display = 'block';
+    } else {
+        itensBurg.style.display = 'block';
+        barra.style.display = 'none';
+        fechar.style.display = 'block';
+    }
+}
+
+function clickFechar() {
+    var itensBurg = document.getElementById("itensBurg");
+    var barra = document.getElementById("barra");
+    var fechar = document.getElementById("fechar");
+    
+    itensBurg.style.display = 'none';
+    barra.style.display = 'block';
+    fechar.style.display = 'none';
+}
